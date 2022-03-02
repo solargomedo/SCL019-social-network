5e217733359b7b1e11de3bc1576190fd04be2e23
Solange Argomedo <solangeargomedoa@gmail.com>
2022-03-02 14:27:02 -0300

    firebase

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,51 @@
-// aqui exportaras las funciones que necesites
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js';
+import { getFirestore, collection} from 'https://www.gstatic.com/firebasejs/9.6.7/firebase-firestore.js';
+import {
+  getAuth,
+  createUserWithEmailAndPassword, 
+  // signOut,
+   signInWithEmailAndPassword,
+   sendEmailVerification,
+  //  onAuthStateChanged,
+   GoogleAuthProvider,
+  //  getRedirectResult,
+  // signInWithRedirect,
+  signInWithPopup
+}
 
-export const myFunction = () => {
-  // aqui tu codigo
-  console.log('Hola mundo!');
+// Your web app's Firebase configuration
+from firebaseConfig = {
+  apiKey: 'AIzaSyBUvj7Ny-b2QLU57iZ_B0CP8gsezTwV75Q',
+  authDomain: 'social-network-949f0.firebaseapp.com',
+  projectId: 'social-network-949f0',
+  storageBucket: 'social-network-949f0.appspot.com',
+  messagingSenderId: '499338039893',
+  appId: '1:499338039893:web:7f04fc4790a65d8949fadc',
 };
+
+// Initialize Firebase
+const auth = getAuth();
+const db = getFirestore();
+db.collection("todos").getDocs();
+const todosCol = collection(db, "todos");
+const snapshot = await getDocs(todosCol);
+
+
+//minuto 5, aun falta
+//detect outh slate
+//auth.onAuthStateChanged (user) => {
+
+
+
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid;
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
