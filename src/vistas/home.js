bf36a249131277d78e8ab96ad66f3faaa258459d
Solange Argomedo <solangeargomedoa@gmail.com>
2022-03-07 09:41:00 -0300

    router

--- src/vistas/home.js
+++ src/vistas/home.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export const home = ()=>{
 const viewHome = document.createElement("div");
 const formularioHome = `<form id = "formularioAcceso" class = "formularioAcceso">
@@ -14,25 +13,3 @@ viewHome.innerHtml= formularioHome;
 
 return viewHome
 }
-=======
-// export const home = ()=>{
-// const viewHome = document.createElement("div");
-// const formularioHome = `<form id = "formularioAcceso" class = "formularioAcceso">
-// <div class= "email> Email </div>
-// <input type = "email" id= "emailAcceso" class= "emailAcceso" placeholder="Email"/>
-// <div class= "password"> Password </div>
-// <input type="password" id="passwordAcceso" class="passwordAcceso" placeholder= "Enter password"/>
-// button type="button" name="registrar" id="enviarRegistro" class="enviarRegistro" /button>
-// </form>`
-// viewHome.innerHtml= formularioHome;
-
-
-
-// return viewHome
-// }
-
-
-
-
-
->>>>>>> c9b5f3a9f81578ecc4fa6bc2d6ffffec4aff6daa
