82b0fee7c045460bf23af2480714a5298555accc
MonicaCofre <cofrev@gmail.com>
2022-03-08 23:51:25 -0300

    rutas home

--- src/vistas/home.js
+++ src/vistas/home.js
@@ -1,15 +1,31 @@
 export const home = ()=>{
-const viewHome = document.createElement("div");
-const formularioHome = `<form id = "formularioAcceso" class = "formularioAcceso">
-<div class= "email> Email </div>
-<input type = "email" id= "emailAcceso" class= "emailAcceso" placeholder="Email"/>
-<div class= "password"> Password </div>
-<input type="password" id="passwordAcceso" class="passwordAcceso" placeholder= "Enter password"/>
-button type="button" name="registrar" id="enviarRegistro" class="enviarRegistro" /button>
-</form>`
-viewHome.innerHtml= formularioHome;
+    const userData = document.createElement('div');
+  userData.className = 'userData';
+  userData.setAttribute("class", "mainclass");
+  const data = `
+  <form id="formLogin" class="FormLogin">
+  <div class="name"> Name </div>
+  <input type="text" id="registerNameLogin" class="registerNameLogin" placeholder= "Enter your name"/>
+  <div class="nickName"> Nickname </div>
+  <input type="text" id="nickNameLogin" class="nickNameLogin" placeholder= "Enter your nickname"/>
+  <div class="email"> E-mail </div>
+  <input type="email" id="emailLogin" class="emailLogin" placeholder= "Enter e-mail"/>
+  <div class="password"> Password </div>
+  <span class="iconEye"><i id="checkEye" class="fas fa-eye-slash"></i></span>
+  <input type="password" id="passwordLogin" class="passwordLogin" placeholder= "Enter password"/>
+  <input type="submit" id="submit-register" class="submit-register" value="Continue">
+  </form>
+  </div>
+  `;
+ userData.innerHTML = data;
 
-
-
-return viewHome
+ window.location.hash = 'home';
+ return userData
 }
+
+
+
+
+
+
+
