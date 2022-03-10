0f55cbc5d2684dbea6f2ce1b37a4481b81fd4cda
Solange Argomedo <solangeargomedoa@gmail.com>
2022-03-10 09:31:02 -0300

    rutas register y home ok

--- src/lib/routes.js
+++ src/lib/routes.js
@@ -1,36 +1,41 @@
-import { login } from '../vistas/login.js';
-/*import { register } from '../vistas/register.js';*/
+//import { login } from '../vistas/login.js';
+import { register } from '../vistas/register.js';
 /*import { post } from '../vistas/post.js';*/
 import { home } from '../vistas/home.js';
 
-const showTemplate = (hash) =>{
- 
+const showTemplate = (hash) => {
+
   const router = hash.substring(0);
   const containerRoot =
-      document.getElementById('root');
+    document.getElementById('root');
   containerRoot.innerHTML = '';
 
-  console.log("valor = " + window.location);
+  console.log("valor = " + router);
   switch (router) {
+    case "":
 
-      case '#home':
-          containerRoot.appendChild(home())
-          break;
-      case '#login':
-          containerRoot.appendChild(login())
-          break;
-      default:
-        console.log("valor = " + router);
-          containerRoot.innerHTML = `<p> Error 404 + router </p>`
+    case '#home':
+      containerRoot.appendChild(home())
+      break
+
+    case '#register':
+      containerRoot.appendChild(register())
+      break;
+
+    default:
+      console.log("valor = " + router);
+      containerRoot.innerHTML = `<p> Ups!algo salio mal </p>`
   }
 }
 
 export const initRouter = () => {
   window.addEventListener('load', showTemplate(window.location.hash));
+  console.log(showTemplate)
+  console.log(showTemplate(window.location.hash))
   if ('onhashchange' in window) {
-      window.onhashchange = () => {
-          showTemplate(window.location.hash);
-      }
+    window.onhashchange = () => {
+      showTemplate(window.location.hash);
+    }
   }
 }
 
