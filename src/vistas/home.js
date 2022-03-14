9d40a95a22c12801bef1b46b6fd760e733464b1d
MonicaCofre <cofrev@gmail.com>
2022-03-14 09:00:00 -0300

    responsive

--- src/vistas/home.js
+++ src/vistas/home.js
@@ -3,17 +3,22 @@ export const home = ()=>{
   userData.className = 'userData';
   userData.setAttribute("class", "mainclass");
   const data = `
+  <div class="titulo"> Te apaño </div>
+ 
   <form id="formLogin" class="FormLogin">
-  <div class="email"> E-mail </div>
+  <div class="email"> Correo electrónico </div>
   <input type="email" id="emailLogin" class="emailLogin" placeholder= "Ingresa tu e-mail"/>
   <div class="password"> Contraseña </div>
   <span class="iconEye"><i id="checkEye" class="fas fa-eye-slash"></i></span>
   <input type="password" id="passwordLogin" class="passwordLogin" placeholder= "Ingresa tu clave"/>
-  <input type="submit" id="submit-register" class="submit-register" value="ingresa">
+  <input type="submit" id="submit-register" class="submit-register" value="Ingresar">
   <button id="boton" class="boton">Ingresa con Google</button>
   <div class="registrate"> ¿No tienes cuenta?</div>
   <button id="boton" class="boton">Registrate</button>
   </form>
+  <div class="imagen">
+  <img src="../Imagenes/deportesvec.jpg"> </div>
+    
   </div>
   `;
  userData.innerHTML = data;
--- src/vistas/register.js
+++ src/vistas/register.js
@@ -16,13 +16,13 @@ infoContainer.appendChild(registerText);*/
   userData.setAttribute("class", "mainclass");
   const data = `
   <form id="formLogin" class="FormLogin">
-  <div class="name"> Name </div>
-  <input type="text" id="registerNameLogin" class="registerNameLogin" placeholder= "Enter your name"/>
-  <div class="email"> E-mail </div>
-  <input type="email" id="emailLogin" class="emailLogin" placeholder= "Enter e-mail"/>
-  <div class="password"> Password </div>
+  <div class="name"> Nombre </div>
+  <input type="text" id="registerNameLogin" class="registerNameLogin" placeholder= "Ingresa tu nombre"/>
+  <div class="email"> Correo Electrónico </div>
+  <input type="email" id="emailLogin" class="emailLogin" placeholder= "Ingresa tu e-mail"/>
+  <div class="password"> Contraseña </div>
   <span class="iconEye"><i id="checkEye" class="fas fa-eye-slash"></i></span>
-  <input type="password" id="passwordLogin" class="passwordLogin" placeholder= "Enter password"/>
+  <input type="password" id="passwordLogin" class="passwordLogin" placeholder= "Ingresa tu clave"/>
   <input type="submit" id="submit-register" class="submit-register" value="Registrarse">
   </form>
   </div>
