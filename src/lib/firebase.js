7755065a4696c0f194b8058ff55be7962b9d75b3
Solange Argomedo <solangeargomedoa@gmail.com>
2022-03-28 09:47:58 -0300

    Entrar

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,10 +1,7 @@
-// Import las funciones de Firebase (SDK) (todo copiado desde web)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js";
+import { collection, addDoc, getFirestore, getDocs} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js"
+
 const firebaseConfig = {
   apiKey: "AIzaSyBIqyphHuzt--s38OayJrvHpQl11oY8Fw0",
   authDomain: "appsocialnetwork-45b2a.firebaseapp.com",
@@ -17,4 +14,24 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
+import { collection, addDoc, getDocs } from "firebase/firestore";
+
+// Add a new document with a generated id.
+export const createPost = async(comentario, titulo)=>{
+const docRef = await addDoc(collection(db, "post"), {
+  name: titulo,
+  country: comentario
+});
+console.log("Document written with ID: ", docRef.id);
+}
+
+export const getAllPost = async() =>{
+const allpost = await getDocs(collection(db, "post"));
+allpost.forEach((doc) => {
+console.log(doc, id, "=>", doc.data());
+
+});
+}
 
--- src/vistas/post.js
+++ src/vistas/post.js
@@ -1,27 +1,101 @@
-export const post = () => {
+import { createPost, db } from "../../lib/firebase.js";
+import { collection, getDocs, orderBy,} from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js';
+
+
+ export const post =  () => {  
+ 
   const dataPost = document.createElement('div');
   dataPost.setAttribute("class", "mainclass");
   const data = `
+    
     <main>
-    <section  class= "contentBox">
-    <div class="iconshome">
-    <a span class="icon icon-home3" href="#muro">Muro</a>
+    <form id="task-form">
+    <label for="title"> title:</label>
+    <input type="text" placeholder="task title" id= "task-title">
+    
+    <label for="description"> description:</label>
+    <textarea id="task-description" rows="3" placeholder= "task description"></textarea>
+    
+    <button id="btn-task-save">save</button>
+
+    </form>
+    
     <button class="returnBtn" type="submit">
     <a href="#home" type="button" id="btnCerrar" class="btnCerrar">Cerrar sesion</a>
-    </div>
-    </section>
-    <section id="contenedor-perfil" class="contenedor-perfil"
-    <article class="recet">
-    <input type="search" class="textarea" id="NamePost" placeholder="¿Cual es tu nombre?"/>
-    <input type="search" class="textarea" id="tipPost" placeholder="Dejanos tu tips saludable"/>
-    <input type="search" class="textarea" id="post" placeholder="¿Me apañas a entrenar?"/>
-    <button class="botonPost" id="publicar">Publicar</button>  
-    </section>
+    <div id="tesks-container"></div>
+
     </main>`;
 
-  dataPost.innerHTML = data;
+    dataPost.innerHTML = data;
 
-  window.location.hash = 'post';
-  return dataPost
 
-  }
+
+    //btn addPost
+const btnAddPost = dataPost.querySelector("#btn-task-save"); //--------Evento boton registrarse
+let btonPost = dataPost.querySelector('#btn-task-save');       
+btonPost.addEventListener('click', () => {                       
+ postUsuario();                                                 
+});
+btnAddPost.addEventListener("click", async(event) => {
+    event.preventDefault();
+    //almacena el comentario
+    const publication = dataPost.querySelector("#task-description").value;
+    console.log(publication);
+    //llamar a createpost
+    await createPost(publication);
+});
+
+const getAllPost = async() => {
+
+    const allpost = await getDocs(collection(db, "post"));
+    allpost.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        //console.log(doc.id, " => ", doc.data());
+
+        const containerPost = dataPost.querySelector('#tesks-container')
+        const timer = query(collection(db, "post"), orderBy("date", "desc"))
+
+        onSnapshot(timer, (querySnapshot) => {
+            let html = ''
+
+            querySnapshot.forEach(doc => {
+                const commentPost = doc.data();
+                html += `
+        
+          <div> 
+          <h3 class="titlePost">${commentPost.title}</h3>
+          <textarea class="commentDone" readonly>${commentPost.description}</textarea>
+       
+          <div class="btns"> 
+            <input class="counter" id="counter" type="number"  value="0" name="" readonly  />
+            
+            <button class="like" id="like"><i class="fa-solid fa-heart"></i></button> 
+            <button class="btnDelete" data-id="${doc.id}"><i class="fa-solid fa-trash"></i>
+                Delete</button>
+            <button class="btnEdit" data-id="${doc.id}"><i class="fa-solid fa-pen-to-square"></i>
+                Edit</button>
+          </div> 
+        </div>`
+            })
+        })
+    });
+ 
+    }
+    
+    
+    window.location.hash = 'post';
+
+    return dataPost
+
+
+    
+    }
+
+   
+ 
+    
+    
+  
+    
+
+    
