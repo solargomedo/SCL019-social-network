944e898f7b74cd221327b852e13053c60ee3cb31
GabrielaRomeroMolina <gaba.rome@gmail.com>
2022-03-30 11:13:41 -0300

    cambios css

--- src/vistas/post.js
+++ src/vistas/post.js
@@ -2,22 +2,23 @@ export const post = () => {
   const dataPost = document.createElement('div');
   dataPost.setAttribute("class", "mainclass");
   const data = `
-    <main>
-    <section  class= "contentBox">
-    <div class="iconshome">
+    <div class= mainclass>
+    <div class = "header">
+    <div class="logo">
+    <img class="img" src="../Imagenes/mano1.png">  
+    </div>
+    <h1>Te Apaño!</h1>
+    </div>
+    <form id="formLogin" class= "FormLogin">
     <a span class="icon icon-home3" href="#muro">Muro</a>
     <button class="returnBtn" type="submit">
     <a href="#home" type="button" id="btnCerrar" class="btnCerrar">Cerrar sesion</a>
-    </div>
-    </section>
-    <section id="contenedor-perfil" class="contenedor-perfil"
-    <article class="recet">
     <input type="search" class="textarea" id="NamePost" placeholder="¿Cual es tu nombre?"/>
     <input type="search" class="textarea" id="tipPost" placeholder="Dejanos tu tips saludable"/>
     <input type="search" class="textarea" id="post" placeholder="¿Me apañas a entrenar?"/>
     <button class="botonPost" id="publicar">Publicar</button>  
-    </section>
-    </main>`;
+    </form>
+    </div>`;
 
   dataPost.innerHTML = data;
 
