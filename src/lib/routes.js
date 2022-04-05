fbd1200b1a34505517237fe693ce7577e04f97bc
Solange Argomedo <solangeargomedoa@gmail.com>
2022-04-05 08:57:24 -0400

    Entrar

--- src/lib/routes.js
+++ src/lib/routes.js
@@ -2,46 +2,41 @@ import { register } from '../vistas/register.js';
 import { post } from '../vistas/post.js';
 import { home } from '../vistas/home.js';
 
-/*mostrar plantillas */
+/* mostrar plantillas */
 const showTemplate = (hash) => {
+  const router = hash.substring(0);
+  const containerRoot = document.getElementById('root');
+  containerRoot.innerHTML = '';
 
-  const router = hash.substring(0); 
-    const containerRoot =
-    document.getElementById('root');
-    containerRoot.innerHTML = '';
-
-  //console.log("valor = " + router);
+  // console.log("valor = " + router);
   switch (router) {
-    case "":
+    case '':
 
     case '#home':
-      containerRoot.appendChild(home()) 
-      break
+      containerRoot.appendChild(home());
+      break;
 
     case '#register':
-      containerRoot.appendChild(register())
+      containerRoot.appendChild(register());
       break;
 
     case '#post':
-      containerRoot.appendChild(post())
+      containerRoot.appendChild(post());
       break;
 
     default:
-      console.log("valor = " + router);
-      containerRoot.innerHTML = `<p> Ups!algo salio mal </p>`
+      console.log(`valor = ${router}`);
+      containerRoot.innerHTML = 'Ups!algo salio mal';
   }
-}
+};
 
 export const initRouter = () => {
   window.addEventListener('load', showTemplate(window.location.hash));
-  //console.log(showTemplate)
-  //console.log(showTemplate(window.location.hash))
+  // console.log(showTemplate)
+  // console.log(showTemplate(window.location.hash))
   if ('onhashchange' in window) {
     window.onhashchange = () => {
       showTemplate(window.location.hash);
-    }
+    };
   }
-}
-
-
-
+};
